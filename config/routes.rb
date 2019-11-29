21f647c5ccf06a7d8ac8a59f716de148643d7faa
UBUNTU <softices@softices-Latitude-E6230>
2019-11-29 12:44:21 +0530

    activeadmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :categories
-  resources :reviews 
+  resources :reviews
   get 'home/index'
   devise_for :users
   resources :books do
