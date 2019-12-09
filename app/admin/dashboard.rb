9384a8bdfa739d118985f449c2203ca58140fa96
parth <parthu1213@gmail.com>
2019-12-09 16:30:30 +0530

    Book,User,Reviews Counts

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,9 +4,27 @@ ActiveAdmin.register_page "Dashboard" do
   content title: proc { I18n.t("active_admin.dashboard") } do
     div class: "blank_slate_container", id: "dashboard_default_message" do
       span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+        span "Users"
+        small User.count
       end
+
+      span class: "blank_slate" do
+        span "Books"
+        small Book.count
+      end
+
+      span class: "blank_slate" do
+        span "Reviews"
+        small Review.count
+      end
+      # span class: "blank_slate" do
+      #   span Books
+      #   small Book.count
+      # end
+      # span class: "blank_slate" do
+      #   span Reviews
+      #   small Review.count
+      # end
     end
 
     # Here is an example of a simple dashboard with columns and panels.
